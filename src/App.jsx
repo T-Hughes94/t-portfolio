bffc0654b244149c75a275419012fefc22e38ef1
Terence Hughes <Terencehughes450@gmail.com>
2025-03-02 23:09:32 -0500

    setup component tree and boiler plate code for each. linked all to app level

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar'
+import AboutMe from './AboutMe'
+import Experience from './Experience'
+import TechnicalSkills from './TechnicalSkills'
+import Education from './Education'
+import Projects from './Projects'
+import Hero from './Hero'
+import Footer from './Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,9 +16,14 @@ function App() {
   return (
     <>
       <div>
-        <h1>
-          Hello World
-        </h1>
+        <Navbar />
+        <Hero />
+        <AboutMe />
+        <TechnicalSkills />
+        <Projects />
+        <Experience />
+        <Education />
+        <Footer />
       </div>
     </>
   )
