e7fd05081c1534d48b1b22196fe77565fa496ba4
Terence Hughes <Terencehughes450@gmail.com>
2025-03-04 22:28:10 -0500

    Working on about component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
     content: [
       "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}", // ✅ Ensure Tailwind scans all JSX/TSX files
+      "../src/**/*.{js,ts,jsx,tsx}", // ✅ Ensure Tailwind scans all JSX/TSX files
     ],
     theme: {
       extend: {},
